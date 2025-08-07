0017537827693f606c639df4f77311cbd6e7f197
ahmvddddd <belloahmadmm@gmail.com>
2025-08-07 01:17:50 +0000

    firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,34 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:myapp/firebase_options.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
+  runZonedGuarded(
+    () async {
+  await dotenv.load(fileName: ".env");
+      WidgetsFlutterBinding.ensureInitialized();
+
+      if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp(
+      name: "vider_client",
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
+  debugPrint(' initialized firebase apps: ${Firebase.apps}');
+
+
+      
   runApp(ProviderScope(child: App()));
+  },
+    (error, stackTrace) {
+      throw error;
+    },
+  );
 }
 
